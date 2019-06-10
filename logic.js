06f0ff6ae92b9fd2aaa60852896d95dc0360fe40
Nick Kinney <Nick.Kinney@sabre.com>
2019-06-10 09:28:59 -0500

    Added sphere, improved viewing experience

--- logic.js
+++ logic.js
@@ -7,8 +7,8 @@ let dims;
 let time = 0;
 let balls;
 let numBalls = 200;
-let maxSpeed = 2;
-
+let maxSpeed = 4;
+let radius = 400;
 var backgroundImage;
 
 function setup(){
@@ -28,18 +28,21 @@ function draw(){
     orbitControl();  
     background(0);
     pointLight(255,255,255,0,0,0);
+    stroke(64,64,64,16);
+    noFill();
+    ellipsoid(radius, radius, radius);
     balls.forEach((ball) => {
         const noiseSeed = createVector(ball.pos.x/dims.x*increment.x+offSetStart.x, ball.pos.y/dims.y*increment.y+offSetStart.y, ball.pos.z/dims.z*increment.z+offSetStart.z);
         const noiseAngleRaw = noise(noiseSeed.x, noiseSeed.y, noiseSeed.z);
         const noiseAngle2Raw = noise(noiseSeed.x*PI+100000, noiseSeed.y*PI + 1000000, noiseSeed.z*PI + 10000000);
-        const noiseAngle = map(noiseAngleRaw, 0, 1, -PI, PI);
-        const noiseAngle2 = map(noiseAngle2Raw, 0, 1, -PI, PI);
+        const noiseAngle = map(noiseAngleRaw, 0, 1, -8*PI, 8*PI);
+        const noiseAngle2 = map(noiseAngle2Raw, 0, 1, -8*PI, 8*PI);
         const noiseVector = p5.Vector.fromAngles(noiseAngle, noiseAngle2);
         ball.v = noiseVector.mult(maxSpeed);
         ball.move();
         ball.draw();
-        if(ball.pos.mag() >= 400){
-            ball.pos = p5.Vector.fromAngles(random(2*PI), random(2*PI)).mult(random(400));
+        if(ball.pos.mag() >= radius){
+            ball.pos = p5.Vector.fromAngles(random(2*PI), random(2*PI)).mult(random(radius));
             ball.v = ball.v.mult(0);
         }
     });
