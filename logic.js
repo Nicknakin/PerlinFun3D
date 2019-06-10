f14e59e85640f51c1d44d4197bfc00b92d942362
Nick Kinney <Nick.Kinney@sabre.com>
2019-06-10 09:14:35 -0500

    Some cleanup

--- logic.js
+++ logic.js
@@ -7,31 +7,31 @@ let dims;
 let time = 0;
 let balls;
 let numBalls = 200;
-let maxSpeed = 1;
+let maxSpeed = 2;
 
 var backgroundImage;
 
 function setup(){
-    var canvas = createCanvas(windowWidth, windowHeight, WEBGL);
+    var canvas = createCanvas(windowWidth*0.95, windowHeight*0.95, WEBGL);
     offSet = createVector(0,0,0);
     offSetStart = createVector(100,100,100);
-    increment = createVector(0.01, 0.01, 0.01);
+    increment = createVector(0.001, 0.001, 0.001);
     dims = createVector(1,1,1);
 
     balls = new Array(numBalls).fill().map(() =>  randomBall());
 
-    noiseDetail(8);
+    noiseDetail(4);
     background(0);
 }
 
 function draw(){
     orbitControl();  
     background(0);
+    pointLight(255,255,255,0,0,0);
     balls.forEach((ball) => {
         const noiseSeed = createVector(ball.pos.x/dims.x*increment.x+offSetStart.x, ball.pos.y/dims.y*increment.y+offSetStart.y, ball.pos.z/dims.z*increment.z+offSetStart.z);
-        
         const noiseAngleRaw = noise(noiseSeed.x, noiseSeed.y, noiseSeed.z);
-        const noiseAngle2Raw = noise(noiseSeed.x+100000, noiseSeed.y + 1000000, noiseSeed.z + 10000000);
+        const noiseAngle2Raw = noise(noiseSeed.x*PI+100000, noiseSeed.y*PI + 1000000, noiseSeed.z*PI + 10000000);
         const noiseAngle = map(noiseAngleRaw, 0, 1, -PI, PI);
         const noiseAngle2 = map(noiseAngle2Raw, 0, 1, -PI, PI);
         const noiseVector = p5.Vector.fromAngles(noiseAngle, noiseAngle2);
